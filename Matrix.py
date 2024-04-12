a6e8d184e90a53ce3966aae95e2f8a5214cb0c7b
DmitriyPanfilov <126699728+DmitriyPanfilov@users.noreply.github.com>
2024-04-12 13:50:19 +0300

    Добавлено логировние и обработка ошибок

--- Matrix.py
+++ Matrix.py
@@ -4,20 +4,43 @@
 # командной строки с передачей параметров
 
 
+import logging
+
+logging.basicConfig(filename='Log.log',
+                    filemode='a',
+                    encoding='utf-8',
+                    format='{asctime} {levelname:<8} функция "{funcName}()": {msg}',
+                    style='{',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 
 def transpose(matrix):
-    trans_matrix = [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
-    print(f'Исходная матрица: \n {matrix}\n результат: \n {trans_matrix}')
 
+    trans_matrix = matrix
+    temp = True
+    line_length = (len(matrix[0]))
+    for i in trans_matrix:
+        if line_length != (len(i)):
+            temp = False
+
+    if temp == True:
+        trans_matrix = [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
+        logger.info(f'Транспорирование исходной матрицы {matrix} в матрицу {trans_matrix}')
+        return trans_matrix
+    else:
+        logger.error(f'Невозможно провести транспорирование матрицы {IndexError} Количество индексов матрицы не совпадает')
+        return IndexError("Работа завершина по причине не соовтветствия количества элементов матрицы")
 
 
 
 if __name__ == '__main__':
 
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
+    print(transpose(matrix = [[1, 2, 3],
+                              [4, 5, 6],
+                              [7, 8, 9],
+                              [3, 2, 1]]))
 
 
-    transposed_matrix = transpose(matrix)
